f0e46ebd50ce82b37fc7c117e3bf2f0dcb10a7cb
morninng <mr_morninng_star_dash@yahoo.co.jp>
2014-12-27 05:30:43 +0900

    role register for event

--- view/EventContext.js
+++ view/EventContext.js
@@ -5,23 +5,56 @@ var EventContext = Backbone.View.extend({
 
 	initialize:  function(options){
 		self=this;
-		var event_id = self.model.id;
-		console.log("Event Context view");
-		console.log(event_id);
 
 		var hangout_url = self.model.get("hangout_url");
 		console.log(hangout_url);
 		self.showEvent();
+		self.isHangoutButton = false;
+
 
 		self.render();
 		return self;
 	},
 
+	events: {
+		'click #join_PM': 'Participate_PM',
+		'click #join_LO': 'Participate_LO',
+		'click #join_MG': 'Participate_MG',
+		'click #join_MO': 'Participate_MO',
+		'click #join_RLO': 'Participate_RLO',
+		'click #join_RPM': 'Participate_RPM',
+	},
+
+	Participate_PM: function(){
+
+		var currentUser = Parse.User.current();
+		self.model.set("PrimeMinister",currentUser);
+		self.model.fetch().then(function(event_obj){
+			if(event_obj.get("PrimeMinister")){
+				$("span#event_PM").append("<p>Prime Minister was already assingned to others. please register other role.<p>" );
+
+				return null;
+			}else{
+				self.model.set("PrimeMinister",currentUser);
+				return self.model.save()
+			}
+		}).then(function(){
+			$("span#event_PM").append("<p>you have now joined this event as a Prime Minister<p>");
+		});
+
+		if(!self.isHangoutButton){
+			self.AddHangoutButton();
+		}
+
+	},
+	AddHangoutButton: function(){
+		console.log("add hangout button")
+	},
 	showEvent: function(){
 		self=this;
 		var hangout_domain = self.model.get("hangout_url");
-		console.log(hangout_domain);
 		var currentUser = Parse.User.current();
+		console.log(hangout_domain);
 		var event_id = self.model.id;
 		console.log(event_id);
 
@@ -37,6 +70,9 @@ var EventContext = Backbone.View.extend({
 		self.model.set("hangout_link", hangout_link);
 		var output = self.template({'event_object':self.model.toJSON()});
 		self.$el.html(output);
+
+
+
 	},
 
 	render: function(){
